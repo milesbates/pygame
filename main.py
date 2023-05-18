164e3094f1e079d939c8d5538ecdcf060242c7ea
Miles <milesbates@gmail.com>
2023-05-18 14:59:31 -0500

    New boilerplate.

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
-import sys
-import os
 import pygame
-from pygame.locals import *
- 
-pygame.init()
- 
-fps = 60
-fpsClock = pygame.time.Clock()
- 
-width, height = 640, 480
-screen = pygame.display.set_mode((width, height))
+import sys
 
-class Player(pygame.sprite.Sprite):
-  def __init__(self):
-    points = 0
-   
-# Game loop.
-image_path = os.path.abspath(os.getcwd())+"images/"  # Replace with the path to your image file
-image = pygame.image.load(image_path)
+#Initialize Pygame
+pygame.init()
+
+#Set up the display
+screen_width, screen_height = 1000, 1000
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Sprite Movement")
+
+#Load the image sprite
+sprite_image = pygame.image.load("sprite.png")
+sprite_rect = sprite_image.get_rect()
+sprite_speed = 5
+
+#Set the initial position of the sprite
+sprite_rect.x = screen_width // 2 - sprite_rect.width // 2
+sprite_rect.y = screen_height // 2 - sprite_rect.height // 2
+
+#Game loop
 while True:
-  screen.fill((0, 0, 0))
-  player = Player()   # spawn player
-  player.x = 0   # go to x
-  player.y = 0   # go to y
-  player_list = pygame.sprite.Group()
-  player_list.add(player)
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
-  
-  # Update.
-  
-  # Draw.
-  
-  pygame.display.flip()
-  fpsClock.tick(fps)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Move the sprite
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        sprite_rect.x -= sprite_speed
+    if keys[pygame.K_RIGHT]:
+        sprite_rect.x += sprite_speed
+    if keys[pygame.K_UP]:
+        sprite_rect.y -= sprite_speed
+    if keys[pygame.K_DOWN]:
+        sprite_rect.y += sprite_speed
+
+    # Draw the sprite and update the screen
+    screen.fill((255, 255, 255))
+    screen.blit(sprite_image, sprite_rect)
+    pygame.display.flip()
