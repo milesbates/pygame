d37aa91f95e2e8cdd533e09c4dbac042085914b1
Miles <milesbates@gmail.com>
2023-05-20 13:47:50 -0500

    Added AI image generation, worked on click detect

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import random
 import time
+from pygame.locals import *
 
 #Initialize Pygame
 pygame.init()
@@ -41,8 +42,66 @@ class Pokemon:
         display_text(attack.name + 'missed the opponent!')
     def itemuse(self):
       self.potions-=1
-       
 
+
+class UI:
+  def __init__(self, image, type = "main")  :
+    self.sprite = pygame.transform.scale(pygame.image.load("images/GUI/"+image),(1000,300))
+    self.image = image
+    self.rect = self.sprite.get_rect()
+    self.type = type
+
+  def moveUI(self, image):
+    self.sprite = pygame.transform.scale(pygame.image.load("images/GUI/"+image),(1000,300))
+    self.rect = self.sprite.get_rect()
+    self.rect.x = 0
+    self.rect.y = 700
+  
+  def setImage(self, image):
+     self.sprite = pygame.transform.scale(pygame.image.load("images/GUI/"+image),(1000,300))
+     self.image = image
+     self.rect = self.image.get_rect()
+
+  def onClick(self, x, y):
+    if(self.type == "main" and y <= 850 and y >= 700 ):
+              #fight
+      self.type = "fight"
+      ui.moveUI('EagleMoves.png')
+
+    elif(self.type == "main" and y<=1000 and y >= 850 and x >=0 and x <= 500):
+               #potion
+      self.type = "bag"
+      ui.moveUI('Potion.png')
+
+    elif(self.type == "main" and y<=1000 and y >= 850 and x >=500 and x <= 1000):
+               #run
+      pygame.quit()
+      sys.exit()
+
+    elif(self.type == "fight" and y<=850 and y>=700 and x >=0 and x <= 500):
+      print('top left attack')
+      ui.moveUI('MainUI.png') 
+      self.type = 'main'
+    elif(self.type == 'fight' and y<=850 and y>=700 and x >=500 and x <= 1000):  
+      print('top right attack')  
+      ui.moveUI('MainUI.png')
+      self.type ='main'
+    elif(self.type == 'fight' and y<=1000 and y >= 850 and x >=0 and x <= 500):
+       print('bottom left attack')
+       ui.moveUI('MainUI.png')
+       self.type = 'main'
+    elif(self.type == 'fight' and y<=1000 and y >= 850 and x >=500 and x <= 1000):
+       print('bottom right attack')
+       ui.moveUI('MainUI.png') 
+       self.type ='main'
+    elif(self.type == "bag" and y <= 850 and y >= 700 and x >=0 and x <= 500):
+       print('potion used')
+       ui.moveUI('MainUI.png')
+       self.type = 'main'
+       
+         
+         
+  
 def display_text(text, screen):
    red = (255, 0, 0)
    
@@ -52,20 +111,7 @@ def display_text(text, screen):
    textRect.center = (500, 200)   
    screen.blit(text, textRect)
           
-#DISPLAYING TEXT
-#font = pygame.font.Font('freesansbold.ttf', 32)
-# create a text surface object,
-# on which text is drawn on it.
-#text = font.render('GeeksForGeeks', True, green, blue)
-## create a rectangular object for the
-# text surface object
-# textRect = text.get_rect()
-# # set the center of the rectangular object.
-# textRect.center = (X // 2, Y // 2)
-# copying the text surface object
-# to the display surface object
-# at the center coordinate.
-#
+
 
 class Attack:
     def __init__(self, accuracy, damage, name):
@@ -82,8 +128,7 @@ Eagle = Pokemon(hp = 100, name = "ESD Eagle", moves = EagleMoves, sprite = pygam
 
 LionMoves = [Attack(accuracy = 80, damage = 35, name ='Royal Roar'), Attack(accuracy = 65, damage = 45, name ='Savanna Slam'), Attack(accuracy = 100, damage = 15, name ='Kings Claw'), Attack(accuracy = 50, damage = 50, name ='Pride Pounce')]
 Lion = Pokemon(hp = 100, name = "St. Marks Lion", moves = LionMoves, sprite = pygame.transform.scale(pygame.image.load("images/Sprites/LionSprite.jpg"),(100,100)), potions = 0)
-ui = Pokemon(hp = 0, name = 'ui', moves = [], sprite = pygame.transform.scale(pygame.image.load("images/GUI/MainUI.png"),(1000,300)), potions = 0)
-
+ui = UI(image = 'MainUI.png', type="main")
 
 
 DisplayList = []
@@ -95,9 +140,10 @@ def gameStart(pMon1 = Eagle, pMon2 = Lion):
    
    ui.rect.x = 0
    ui.rect.y = 700
-   DisplayList = [[Eagle.sprite, Eagle.rect], [Lion.sprite,Lion.rect], [ui,ui.rect]]
+  #DisplayList = [[Eagle.sprite, Eagle.rect], [Lion.sprite,Lion.rect], [ui,ui.rect]]
    
 gameStart()
+
 
 
 
@@ -108,34 +154,24 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+           keys = pygame.key.get_pressed()
+           if keys[K_BACKSPACE]:
+              ui.moveUI("MainUI.png")
+              ui.type = "main"
+              
+           
         if event.type == pygame.MOUSEBUTTONDOWN:
           mouse_presses = pygame.mouse.get_pressed()
           if mouse_presses[0]:
             pos = pygame.mouse.get_pos()
             print(pos)
-            if(pos[1] <= 850 and pos[1] >= 700 ):
-               print('fight')#update ui
-            if(pos[1]<=1000 and pos[1] >= 850 and pos[0] >=0 and pos[0] <= 500):
-               print('bag')
-            if(pos[1]<=1000 and pos[1] >= 850 and pos[0] >=500 and pos[0] <= 1000):
-               print('run')
-               
-
-    # Move the sprite
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_LEFT]:
-    #     sprite_rect.x -= sprite_speed
-    # if keys[pygame.K_RIGHT]:
-    #     sprite_rect.x += sprite_speed
-    # if keys[pygame.K_UP]:
-    #     sprite_rect.y -= sprite_speed
-    # if keys[pygame.K_DOWN]:
-    #     sprite_rect.y += sprite_speed
+            ui.onClick(pos[0],pos[1])
+            
 
     # Draw the sprite and update the screen
     screen.fill((255, 255, 255))
-    # for im in DisplayList:
-    #   screen.blit(im[0], im[1])
+    
     
     screen.blit(Eagle.sprite, Eagle.rect)
     screen.blit(Lion.sprite, Lion.rect)
@@ -143,9 +179,21 @@ while True:
     display_text('TEST', screen)
     pygame.display.flip()
 
-#What graphics/UI stuff we need
-# The battle stuff at bottom of the screen
-  #when stuff clicked, changed the bottom of the screen to a new picture
-#Pictures of both "pokemon"
-#Other stuff
+
+
+
+
+#DISPLAYING TEXT
+#font = pygame.font.Font('freesansbold.ttf', 32)
+# create a text surface object,
+# on which text is drawn on it.
+#text = font.render('GeeksForGeeks', True, green, blue)
+## create a rectangular object for the
+# text surface object
+# textRect = text.get_rect()
+# # set the center of the rectangular object.
+# textRect.center = (X // 2, Y // 2)
+# copying the text surface object
+# to the display surface object
+# at the center coordinate.
 #
